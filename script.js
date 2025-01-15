d20beb18db82be5950e0343d2ea657308c149788
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-16 01:24:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function functionName(){
+	alert(arguments.callee.name)
+	return ;
+}
+function AccioJob(arg){
+	alert(arguments.callee.name)
+
+	return ;
+}
+functionName()
+AccioJob()
